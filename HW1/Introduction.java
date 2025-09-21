c1dd127f659fc4f8edcca49ba1f4c4ce12cd49f4
che <stonyyquynh@Stonyys-MacBook-Air-2.local>
2025-09-21 09:49:44 +0700

    Add Introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("Phạm Ánh Chúc\t 24020049 \t K69I-IT4 \t phamanhchuc \t 24020049@vnu.edu.vn \n");
+
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
